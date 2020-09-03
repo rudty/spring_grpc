0f00c79c4dc4b59e6d195b3f9617908ef219a63d
석경우 <rudtyz@gmail.com>
2020-09-03 22:24:10 +0900

    health check

--- src/test/java/org/rudtyz/grpcserver/GreeterServiceTest.java
+++ src/test/java/org/rudtyz/grpcserver/GreeterServiceTest.java
@@ -2,6 +2,9 @@ package org.rudtyz.grpcserver;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.health.v1.HealthCheckRequest;
+import io.grpc.health.v1.HealthCheckResponse;
+import io.grpc.health.v1.HealthGrpc;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,11 +21,15 @@ public class GreeterServiceTest {
     @Value("${grpc.port}")
     int grpcPort = 1234;
 
-    @Test
-    public void helloRequest() throws ExecutionException, InterruptedException {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", grpcPort)
+    private ManagedChannel newManagedChannel() {
+        return ManagedChannelBuilder.forAddress("localhost", grpcPort)
                 .usePlaintext()
                 .build();
+    }
+
+    @Test
+    public void helloRequest() throws ExecutionException, InterruptedException {
+        final ManagedChannel channel = newManagedChannel();
 
         final GreeterGrpc.GreeterFutureStub greeterFutureStub = GreeterGrpc.newFutureStub(channel);
         final HelloRequest helloRequest = HelloRequest.newBuilder()
@@ -33,5 +40,19 @@ public class GreeterServiceTest {
         assertThat(reply).isEqualTo("helloasync");
     }
 
+    @Test
+    public void healthCheck()  throws ExecutionException, InterruptedException {
+        final ManagedChannel channel = newManagedChannel();
 
+        final HealthGrpc.HealthFutureStub healthFutureStub = HealthGrpc.newFutureStub(channel);
+        final HealthCheckRequest healthCheckRequest = HealthCheckRequest.newBuilder()
+                .setService(GreeterGrpc.getServiceDescriptor().getName())
+                .build();
+
+        final HealthCheckResponse.ServingStatus status = healthFutureStub.check(healthCheckRequest)
+                .get()
+                .getStatus();
+
+        assertThat(status).isEqualTo(HealthCheckResponse.ServingStatus.SERVING);
+    }
 }
